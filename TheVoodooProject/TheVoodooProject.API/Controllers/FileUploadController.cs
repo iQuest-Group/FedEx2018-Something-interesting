0e546c424ec3814ec2c684ff68fab9793e4ae888
Eduard Stefanescu <eduard.stefanescu@iquestgroup.com>
2018-11-16 16:48:46 +0200

    Implemented analytics text

--- TheVoodooProject/TheVoodooProject.API/Controllers/FileUploadController.cs
+++ TheVoodooProject/TheVoodooProject.API/Controllers/FileUploadController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Net.Http.Headers;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using TheVoodooProject.API.Models;
 
 namespace TheVoodooProject.API.Controllers
 {
@@ -33,7 +36,7 @@ namespace TheVoodooProject.API.Controllers
 
                 if (file.Length <= 0) return Json("Upload Successful.");
 
-                var fullPath = Path.Combine(newPath, file.FileName.ToString());
+                var fullPath = Path.Combine(newPath, file.FileName);
                 using (var stream = new FileStream(fullPath, FileMode.Create))
                 {
                     file.CopyTo(stream);
@@ -43,6 +46,30 @@ namespace TheVoodooProject.API.Controllers
             catch (Exception ex)
             {
                 return Json("Upload Failed: " + ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public void GetResult()
+        {
+            ITextAnalyticsClient client = new TextAnalyticsClient(new ApiKeyServiceClientCredentials())
+            {
+                Endpoint = "https://westeurope.api.cognitive.microsoft.com"
+            }; //Replace 'westus' with the correct region for your Text Analytics subscription
+
+            SentimentBatchResult result3 = client.SentimentAsync(
+                    new MultiLanguageBatchInput(
+                        new List<MultiLanguageInput>()
+                        {
+                          new MultiLanguageInput("en", "0", "I had the best day of my life."),
+                          new MultiLanguageInput("en", "1", "This was a waste of my time. The speaker put me to sleep."),
+                          new MultiLanguageInput("es", "2", "No tengo dinero ni nada que dar..."),
+                          new MultiLanguageInput("it", "3", "L'hotel veneziano era meraviglioso. È un bellissimo pezzo di architettura."),
+                        })).Result;
+
+            foreach (var document in result3.Documents)
+            {
+                Console.WriteLine("Document ID: {0} , Sentiment Score: {1:0.00}", document.Id, document.Score);
             }
         }
     }
---
+++ TheVoodooProject/TheVoodooProject.API/Models/Class.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Rest;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TheVoodooProject.API.Models
+{
+    public class ApiKeyServiceClientCredentials : ServiceClientCredentials
+    {
+        private const string SubscriptionKey = "5968da83bbb54726b00119c219b4f2ff";
+
+        public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Add("Ocp-Apim-Subscription-Key", SubscriptionKey);
+            return base.ProcessHttpRequestAsync(request, cancellationToken);
+        }
+    }
+}
